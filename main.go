ec814ea1d2b63a49dce72d516bed66b00d7d02d4
hermy <hermy@helpingpeoplenow.org>
2026-06-05 13:35:21 +0200

    add random phrase after hello in /api/v1/hello

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 )
@@ -15,14 +16,28 @@ type HelloResponse struct {
 	Message string `json:"message"`
 }
 
+var phrases = []string{
+	"keep going",
+	"you've got this",
+	"make it happen",
+	"stay curious",
+	"build something awesome",
+	"one step at a time",
+	"dream big",
+	"code on",
+	"never stop learning",
+	"be the change",
+}
+
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(HealthResponse{Status: "ok"})
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	phrase := phrases[rand.Intn(len(phrases))]
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(HelloResponse{Message: "hello"})
+	json.NewEncoder(w).Encode(HelloResponse{Message: "hello, " + phrase})
 }
 
 func main() {
